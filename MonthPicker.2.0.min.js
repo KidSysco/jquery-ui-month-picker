a8029159f078d1b629742f8b1c6c84efd2e20a1b
Ryan Segura <kidsysco@gmail.com>
2013-03-24 09:00:28 -0500

    Update MonthPicker.2.0.min.js

--- MonthPicker.2.0.min.js
+++ MonthPicker.2.0.min.js
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2013 Ryan Segura - https://github.com/KidSysco/jquery-ui-month-picker/
+https://github.com/KidSysco/jquery-ui-month-picker/
 This library is free software; you can redistribute it and/or
 modify it under the terms of the GNU Lesser General Public
 License as published by the Free Software Foundation;

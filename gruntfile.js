ababc7eeb78847a5803a7a9a1a9f183ec2b1c0a1
Benjamin Albert <benjamin_a5@yahoo.com>
2015-10-23 20:07:59 +0300

    Makes grunt produce a single minified files for the deom

--- gruntfile.js
+++ gruntfile.js
@@ -4,21 +4,37 @@ module.exports = function (grunt) {
   // Project Configuration
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    qunit: {
+    	all: ['test/test.html']
+  	},
+    
     uglify: {
       production: {
         files: {
           'MonthPicker.min.js': 'MonthPicker.js'
         }
+      },
+      
+      demo: {
+	    files: {
+	      'demo/Demo.min.js': ['test/jquery-1.9.1.js', 'test/jquery-ui.js', 'test/jquery.maskedinput.min.js', 'MonthPicker.js']
+	    }
       }
     },
-    qunit: {
-    	all: ['test/test.html']
+    
+    cssmin: {
+	  demo: {
+	     files: {
+		   'demo/Demo.css': ['test/jquery-ui.css', 'css/MonthPicker.css', 'test/test.css']
+	     }
+	  }
   	}
   });
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-  grunt.registerTask('default', ['qunit', 'uglify']);
+  grunt.registerTask('default', ['qunit', 'uglify', 'cssmin']);
   grunt.registerTask('test', ['qunit']);
 };

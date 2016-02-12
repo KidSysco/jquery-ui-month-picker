df63425dae49d127c7375fe933dcc379f841ed5b
Benjamin Albert <benjamin_a5@yahoo.com>
2016-02-12 18:53:16 +0200

    Adds the grunt gh-pages task and updates the license in package.json
    Adds a grunt task that adds all the files in the /demo
    directory to the gh-pages branch and pushes the branch
    to origin (aka GitHub)

--- gruntfile.js
+++ gruntfile.js
@@ -22,12 +22,22 @@ module.exports = function (grunt) {
            'demo/MonthPicker.min.css': 'src/MonthPicker.css'
 	     }
 	  }
-  	}
+    },
+
+    'gh-pages': {
+       options: {
+         base: 'demo',
+         add: true
+       },
+
+       src: ['**']
+    }
   });
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-qunit');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-gh-pages');
 
   grunt.registerTask('default', ['qunit', 'uglify', 'cssmin']);
   grunt.registerTask('test', ['qunit']);

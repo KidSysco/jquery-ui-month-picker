6cdfd890a9a74cc69bedde826381cbfc1587a42f
Benjamin Albert <benjamin_a5@yahoo.com>
2016-04-18 21:47:37 +0300

    Bumps version to 3.0.1

--- src/MonthPicker.js
+++ src/MonthPicker.js
@@ -1,7 +1,7 @@
 /*
 https://github.com/KidSysco/jquery-ui-month-picker/
 
-Version 3.0.0
+Version 3.0.1
 
 This library is free software; you can redistribute it and/or
 modify it under the terms of the GNU Lesser General Public
@@ -162,7 +162,7 @@ http://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt.
     }
 
     $.MonthPicker = {
-        VERSION: '3.0.0', // Added in version 2.4;
+        VERSION: '3.0.1', // Added in version 2.4;
         i18n: {
             year: 'Year',
             prevYear: 'Previous Year',
